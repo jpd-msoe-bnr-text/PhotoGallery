dd4e0825ec2588f1291905d0e37f8b9ae8a54ce0
John DeMastri <demastri@msoe.edu>
2023-10-20 12:46:08 -0500

    Chapter 22 - p250 - after listing 22.7 - extended preferences / worker to track last image seen - notify user via log if a new element is seen

--- app/src/main/java/demastri/msoe/photogallery/PollWorker.kt
+++ app/src/main/java/demastri/msoe/photogallery/PollWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.flow.first
 
 private const val TAG = "PollWorker"
 
@@ -13,6 +14,32 @@ class PollWorker(
 ) : CoroutineWorker(context, workerParameters) {
     override suspend fun doWork(): Result {
         Log.i(TAG, "Work request triggered")
-        return Result.success()
+        val preferencesRepository = PreferencesRepository.get()
+        val photoRepository = PhotoRepository()
+
+        val query = preferencesRepository.storedQuery.first()
+        val lastId = preferencesRepository.lastResultId.first()
+
+        if (query.isEmpty()) {
+            Log.i(TAG, "No saved query, finishing early.")
+            return Result.success()
+        }
+        return try {
+            val items = photoRepository.searchPhotos(query)
+
+            if (items.isNotEmpty()) {
+                val newResultId = items.first().id
+                if (newResultId == lastId) {
+                    Log.i(TAG, "Still have the same result: $newResultId")
+                } else {
+                    Log.i(TAG, "Got a new result: $newResultId")
+                    preferencesRepository.setLastResultId(newResultId)
+                }
+            }
+            Result.success()
+        } catch (ex: Exception) {
+            Log.e(TAG, "Background update failed", ex)
+            Result.failure()
+        }
     }
 }
