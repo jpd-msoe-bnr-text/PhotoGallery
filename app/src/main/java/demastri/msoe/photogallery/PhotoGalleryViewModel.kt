1bdb7db8fe5aede254293cd5bb8872e6f213043d
John DeMastri <demastri@msoe.edu>
2023-10-22 17:56:00 -0500

    Chapter 22 - p254 - after listing 22.19 - added menu item to allow user to toggle app polling, changed to periodic work request and added current poll state to UiState

--- app/src/main/java/demastri/msoe/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/demastri/msoe/photogallery/PhotoGalleryViewModel.kt
@@ -40,10 +40,21 @@ class PhotoGalleryViewModel : ViewModel() {
                 }
             }
         }
+        viewModelScope.launch {
+            preferencesRepository.isPolling.collect { isPolling ->
+                _uiState.update { it.copy(isPolling = isPolling) }
+            }
+        }
     }
 
     fun setQuery(query: String) {
         viewModelScope.launch { preferencesRepository.setStoredQuery(query) }
+    }
+
+    fun toggleIsPolling() {
+        viewModelScope.launch {
+            preferencesRepository.setPolling(!uiState.value.isPolling)
+        }
     }
 
     private suspend fun fetchGalleryItems(query: String): List<GalleryItem> {
@@ -58,4 +69,5 @@ class PhotoGalleryViewModel : ViewModel() {
 data class PhotoGalleryUiState(
     val images: List<GalleryItem> = listOf(),
     val query: String = "",
+    val isPolling: Boolean = false,
 )
